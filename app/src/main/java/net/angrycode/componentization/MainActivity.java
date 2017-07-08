7d072ee2eb6993afa796ccdf4d79174d878992ad
wecodexyz <wecodexyz@gmail.com>
2017-07-08 12:36:19 +0800

    rxjava demo

--- app/src/main/java/net/angrycode/componentization/MainActivity.java
+++ app/src/main/java/net/angrycode/componentization/MainActivity.java
@@ -2,13 +2,70 @@ package net.angrycode.componentization;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.reactivestreams.Publisher;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Flowable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.tv_content);
+        Flowable.just("one", "two", "three", "four", "five", "six")
+                .subscribeOn(Schedulers.computation())
+                .observeOn(Schedulers.single())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+                        Log.e(TAG, s);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(@NonNull Throwable throwable) throws Exception {
+                        Log.e(TAG, throwable.getMessage());
+                    }
+                });
 
+        Flowable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(3000);
+                return "from callable";
+            }
+        }).flatMap(new Function<String, Publisher<String>>() {
+            @Override
+            public Publisher<String> apply(@NonNull String s) throws Exception {
+                return Flowable.just(s + " flat map");
+            }
+        }).subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+                        Log.e(TAG, s);
+                        textView.setText(s);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(@NonNull Throwable throwable) throws Exception {
+                        Log.e(TAG, throwable.getMessage());
+                    }
+                });
     }
 }
