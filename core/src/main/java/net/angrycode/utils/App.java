502596b2ca0fe9d328936abbf419a77f647543be
huangyanglin <wecodexyz@gmail.com>
2017-08-17 11:23:18 +0800

    工具类
    增加获取全局application context工具App

---
+++ core/src/main/java/net/angrycode/utils/App.java
@@ -0,0 +1,27 @@
+package net.angrycode.utils;
+
+import android.app.Application;
+import android.util.Log;
+
+public class App {
+    public static final String TAG = App.class.getSimpleName();
+    public static final Application INSTANCE;
+
+    static {
+        Application app = null;
+        try {
+            app = (Application) Class.forName("android.app.AppGlobals").getMethod("getInitialApplication").invoke(null);
+            if (app == null)
+                throw new IllegalStateException("Static initialization of Applications must be on main thread.");
+        } catch (final Exception e) {
+            Log.e(TAG, "Failed to get current application from AppGlobals." + e.getMessage());
+            try {
+                app = (Application) Class.forName("android.app.ActivityThread").getMethod("currentApplication").invoke(null);
+            } catch (final Exception ex) {
+                Log.e(TAG, "Failed to get current application from ActivityThread." + e.getMessage());
+            }
+        } finally {
+            INSTANCE = app;
+        }
+    }
+}
