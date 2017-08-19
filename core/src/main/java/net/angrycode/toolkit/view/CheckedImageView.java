703acd7fb8f4fc41a20463bcc0524956de3e5a6d
huangyanglin <wecodexyz@gmail.com>
2017-08-19 16:30:56 +0800

    CheckedImageView
    添加一个多状态的ImageView

---
+++ core/src/main/java/net/angrycode/toolkit/view/CheckedImageView.java
@@ -0,0 +1,77 @@
+package net.angrycode.toolkit.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.view.ViewDebug;
+
+import net.angrycode.core.R;
+
+/**
+ * Created by wecodexyz on 2017/8/19.
+ */
+
+public class CheckedImageView extends AppCompatImageView {
+    boolean mChecked = false;
+
+    Drawable mNormalState;
+    Drawable mCheckedState;
+
+    public CheckedImageView(Context context) {
+        super(context);
+    }
+
+    public CheckedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public CheckedImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CheckedImageView, defStyleAttr, 0);
+        mChecked = array.getBoolean(R.styleable.CheckedImageView_ImageChecked, false);
+        int normal = array.getResourceId(R.styleable.CheckedImageView_ImageNormalState, 0);
+        int checked = array.getResourceId(R.styleable.CheckedImageView_ImageCheckedState, 0);
+        if (normal != 0) {
+            mNormalState = ContextCompat.getDrawable(context, normal);
+        }
+        if (checked != 0) {
+            mCheckedState = ContextCompat.getDrawable(context, checked);
+        }
+        setChecked(mChecked);
+        array.recycle();
+    }
+
+    public void toggle() {
+        setChecked(!mChecked);
+    }
+
+    @ViewDebug.ExportedProperty
+    public boolean isChecked() {
+        return mChecked;
+    }
+
+    /**
+     * Sets the checked state of this view.
+     *
+     * @param checked {@code true} set the state to checked, {@code false} to
+     *                uncheck
+     */
+    public void setChecked(boolean checked) {
+        if (mChecked != checked) {
+            mChecked = checked;
+        }
+        if (mChecked) {
+            setImageDrawable(mCheckedState);
+        } else {
+            setImageDrawable(mNormalState);
+        }
+    }
+}
---
+++ core/src/main/java/net/angrycode/toolkit/view/SimpleTextWatcher.java
@@ -0,0 +1,28 @@
+package net.angrycode.toolkit.view;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by wecodexyz on 2017/8/19.
+ */
+
+public class SimpleTextWatcher implements TextWatcher {
+    public SimpleTextWatcher() {
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
